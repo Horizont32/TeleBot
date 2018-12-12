4bb8459b3574c681d6483ef3f03ea82cc2348534
Artem <artbryansk94@gmail.com>
2018-12-12 15:03:06 +0300

    defining /start /help commands

--- bot.py
+++ bot.py
@@ -22,6 +22,11 @@ bot = telebot.TeleBot(config.token)
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
