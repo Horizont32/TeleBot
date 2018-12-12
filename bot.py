0f1b3b051dee590e25aae9817a6d4801de05dc78
Artem <artbryansk94@gmail.com>
2018-12-12 15:05:16 +0300

    defining /start /help commands

--- bot.py
+++ bot.py
@@ -18,14 +18,14 @@ bot = telebot.TeleBot(config.token)
 #     bot.send_message(message.chat.id, message.text)
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
